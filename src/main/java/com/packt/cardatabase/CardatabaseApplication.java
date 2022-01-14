3df614011bed8829f2a9a3de0872f309a66ddc4b
Juan Fourment <fourmentjuan@gmail.com>
2022-01-14 10:20:19 -0300

    Seguridad con autenticacion completada

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -10,44 +10,38 @@ import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
 import com.packt.cardatabase.domain.Owner;
 import com.packt.cardatabase.domain.OwnerRepositoy;
+import com.packt.cardatabase.domain.User;
+import com.packt.cardatabase.domain.UserRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
-	//Inject repositories
-	@Autowired
+	@Autowired	
 	private CarRepository repository;
-	
-	@Autowired
+
+	@Autowired	
 	private OwnerRepositoy orepository;
+
+	@Autowired	
+	private UserRepository urepository;	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 	}
-	@Bean
 	
+	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-			// Add owner objects and save these to db
-			Owner owner1 = new Owner("Jhon", "Johson");
-			Owner owner2 = new Owner("Mary", "Robinson");
+			Owner owner1 = new Owner("John" , "Johnson");
+			Owner owner2 = new Owner("Mary" , "Robinson");
 			orepository.save(owner1);
 			orepository.save(owner2);
 			
-			// Add car object with link to owners and save these to db
-			Car car = new Car("Ford","Mustang","Red","ADF-1121",2017,59000,owner1);
-			repository.save(car);
+			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
+			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
+			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
 			
-			car = new Car("Nissan","Leaf","White","SSJ-3002",2014,29000,owner1);
-			repository.save(car);
-			
-			car = new Car("Toyota","Rpius","Silver","KK-0212",2018,39000, owner2);
-			repository.save(car);
-			
-			car = new Car("Renult","Stepway","Red","AAX-1456",2014,10000, owner2);
-			repository.save(car);
-			
+			urepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
+			urepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
 		};
-	}
-	
-
+	}	
 }
